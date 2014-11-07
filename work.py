3d0f97cdb4c44943898129a5b379dcc1921e4195
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:33:58 +0100

    Cleanup tests

--- work.py
+++ work.py
@@ -1,6 +1,6 @@
-#This file is part project_employee module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
+# This file is part project_employee module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 from trytond.model import ModelSQL, fields
 from trytond.pool import PoolMeta
 from trytond.pyson import Eval
