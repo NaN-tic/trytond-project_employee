e5188c5a2d1a30a9ffa8018c6390baea74a8a2dd
resteve <resteve@zikzakmedia.com>
2015-12-01 11:16:30 +0100

    Tests

--- tests/test_project_employee.py
+++ tests/test_project_employee.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
-# This file is part project_employee module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the project_employee module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class ProjectEmployeeTestCase(unittest.TestCase):
+class ProjectEmployeeTestCase(ModuleTestCase):
     'Test Project Employee module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('project_employee')
-
-    def test0005views(self):
-        'Test views'
-        test_view('project_employee')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'project_employee'
 
 
 def suite():
@@ -27,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         ProjectEmployeeTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
